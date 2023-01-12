7ee5620c98407bc91ad6094d48990ab26f4710d6
Vladimir Krasnov <435335vova@gmail.com>
2023-01-12 11:54:50 +0300

    Создание навигационного графа

---
+++ app/src/main/java/dev/vladimir/mobinttestproject/presentation/CardsFragment.kt
@@ -0,0 +1,6 @@
+package dev.vladimir.mobinttestproject.presentation
+
+import androidx.fragment.app.Fragment
+import dev.vladimir.mobinttestproject.R
+
+class CardsFragment : Fragment(R.layout.fragment_cards)
---
+++ app/src/main/java/dev/vladimir/mobinttestproject/presentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.vladimir.mobinttestproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import dev.vladimir.mobinttestproject.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
